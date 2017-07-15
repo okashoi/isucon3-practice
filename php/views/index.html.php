0b9792e9bf7eec3fd9d484f1c0e2d81a63584eb5
Okada Shohei <okashoicircus0@gmail.com>
2017-07-15 10:00:29 +0000

    ループで preg_split を使わないようにした

--- php/views/index.html.php
+++ php/views/index.html.php
@@ -5,8 +5,7 @@
 <ul id="memos">
 <?php foreach($memos as $memo) { ?>
 <li>
-<?php $fragments = preg_split("/\r?\n/", $memo["content"]); ?>
-<a href="<?php echo uri_for('/memo/') ?><?php echo $memo["id"] ?>"><?php echo $fragments[0] ?></a> by <?php echo $memo["username"] ?> (<?php echo $memo["created_at"] ?>)
+<a href="<?php echo uri_for('/memo/') ?><?php echo $memo["id"] ?>"><?php echo trim(explode("\n", $memo["content"])[0]) ?></a> by <?php echo $memo["username"] ?> (<?php echo $memo["created_at"] ?>)
 </li>
 <?php } ?>
 </ul>
